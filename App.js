1ad3e5a7c79e4321965d56a2970bbf3e950c355c
keji <tomitoyo.93@gmail.com>
2023-07-28 15:55:59 +0100

    img

--- App.js
+++ App.js
@@ -1,34 +1,72 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput  style={styles.textInput} placeholder='Your course goal'/>
-        <Button title='ADD GOAL'/>
-      </View>
-      <View>
-        <Text>List of goals here...</Text>
-      </View>
+        <View  style={styles.imgStyle}>
+          <Image  style={styles.image}  source={require('./assets/illustration.png')} />
+        </View>
+        <View style={styles.text}>
+          <Text style={{fontSize: 40,  fontWeight: 'bold',  textAlign: 'center'}}>Finacial lifeline in your pocket.</Text>
+          <Text style={{fontSize: 14,  textAlign: 'center',  paddingTop: 17, color: 'grey'}}>Apply for loans instantly on your mobile</Text>
+          <View style={styles.inputStlye}>
+          
+        </View>
+        <View style={styles.auth}>
+          <Text style={{fontSize: 20,  fontWeight: 'bold',  textAlign: 'center'}}>Sign In</Text>
+          <TouchableOpacity style={styles.button}>
+              <Text style={{fontSize: 20,   textAlign: 'center', color: '#fff'}}>Continue</Text>
+              <Text style={{fontSize: 20,  fontWeight: 'bold',  textAlign: 'center', color: '#fff'}}>&#x2192;</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+        
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+  paddingHorizontal: 2,
+  flex: 1,
+  justifyContent: 'space-between'
   },
-  inputContainer:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  imgStyle:{
+    height:  100,
+    width: 100
   },
-  textInput:{
-    width: '80%',
-    borderStyle: ' solid ',
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: '8px',
-    marginRight: 8,
-    padding: 8,
+  image:{
+    width: 430,
+    height: 440,
+    resizeMode: 'cover',
   },
+  text: {
+      padding: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    inputStlye: {
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        auth: {
+          width: '100%',
+          paddingHorizontal: 20,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: 20,
+          marginTop: 40,
+        },
+        button: {
+          width:  160,
+          color: 'white',
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          alignItems: 'center',
+          backgroundColor: '#ee73c4',
+          height: 60,
+          borderRadius: 40,
+        },
 });
